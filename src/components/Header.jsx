dc66b9ba1343bffff7742fd47356969a63a50cfe
niki <nikiwone@gmail.com>
2023-10-24 17:38:02 +0300

    init the project and added simple components

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="#">Register</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
